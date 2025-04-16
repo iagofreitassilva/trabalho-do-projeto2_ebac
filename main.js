7cea91a92250cf2b3fbf1cf38560fae9737e867e
Iago <iago.f.silva@hotmail.com>
2025-04-16 18:16:55 -0300

    ajustando js

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const form = document.getElementById('form-agenda');
+var linhas = "";
 form.addEventListener('submit',function(e) {
     e.preventDefault();
 
     const inputNomeContato  = document.getElementById('nome-contato');
-    const inpuNumeroContato = document.getElementById('numero-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
 
     let linha = '<tr>';
     linha    += `<td>${inputNomeContato.value}</td>`;
     linha    += `<td>${inputNumeroContato.value}</td>`;
-    linha    += `<td>${inputNomeContato.value = String ? 'Salvo' : 'Não salvo'}</td>`;
+    linha    += `<td>${(typeof inputNomeContato.value) == "string" ? 'Salvo' : 'Não salvo'}</td>`;
     linha    += '</tr>';
 
+    linhas+= linha;
     const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linha;
+
+    corpoTabela.innerHTML = linhas;
 
 });
